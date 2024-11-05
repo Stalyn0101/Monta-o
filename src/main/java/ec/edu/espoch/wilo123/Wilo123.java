256b7b1c4ec643ae715688b8bc04647fe4b0862a
Stalyn0101 <yixon11@gmail.com>
2024-11-05 17:15:07 -0500

    hola

---
+++ src/main/java/ec/edu/espoch/wilo123/Wilo123.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.wilo123;
+
+/**
+ *
+ * @author USER
+ */
+public class Wilo123 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
